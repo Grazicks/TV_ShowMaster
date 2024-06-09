7987974844b6a85682ba1b2e77102f8782495425
Grazicks <rodion.tarasov.03@mail.ru>
2024-06-09 21:15:58 +0700

    Displaying Image Slider Using ViewPager2 & Data Binding(Add image slider in details page)

--- app/src/main/java/com/example/tv_shows_master/activities/TVShowDetailsActivity.java
+++ app/src/main/java/com/example/tv_shows_master/activities/TVShowDetailsActivity.java
@@ -1,13 +1,14 @@
 package com.example.tv_shows_master.activities;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.tv_shows_master.R;
+import com.example.tv_shows_master.adapters.ImageSliderAdapter;
 import com.example.tv_shows_master.databinding.ActivityTvshowDetailsBinding;
 import com.example.tv_shows_master.viewmodels.TVShowDetailsViewModel;
 
@@ -31,8 +32,20 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         tvShowDetailsViewModel.getTVShowDetails(tvShowId).observe(
                 this, tvShowDetailsResponse -> {
                     activityTvshowDetailsBinding.setIsLoading(false);
-                    Toast.makeText(getApplicationContext(),tvShowDetailsResponse.getTvShowDetails().getUrl(), Toast.LENGTH_SHORT).show();
+                    if (tvShowDetailsResponse.getTvShowDetails() != null) {
+
+                        if (tvShowDetailsResponse.getTvShowDetails().getPictures() != null) {
+                            /*--------------------------------------------get Slider images from API ----------------------------------------------------*/
+                            loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
+                        }
+                    }
                 });
 
+    }
+    private void loadImageSlider(String[] sliderImages) {
+        activityTvshowDetailsBinding.sliderViewPager.setOffscreenPageLimit(1);
+        activityTvshowDetailsBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
+        activityTvshowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
+        activityTvshowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
     }
 }
