5b9d1dfc813561b2e8d772749f6858540083f0ff
Grazicks <rodion.tarasov.03@mail.ru>
2024-06-09 19:59:56 +0700

    TV Show Details API Integration (add models:Episode and TVShowDetails )

---
+++ app/src/main/java/com/example/tv_shows_master/models/Episode.java
@@ -0,0 +1,33 @@
+package com.example.tv_shows_master.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Episode {
+    @SerializedName("season")
+    private String season;
+
+    @SerializedName("episode")
+    private String episode;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("air_date")
+    private String airDate;
+
+    public String getSeason() {
+        return season;
+    }
+
+    public String getEpisode() {
+        return episode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAirDate() {
+        return airDate;
+    }
+}
