6728835c744392954f394f57c0aadc6751d2cfb5
Grazicks <rodion.tarasov.03@mail.ru>
2024-06-09 22:57:42 +0700

    Room - Database Setup and Creating Watchlist (creating dao and TVShowdatabase)

---
+++ app/src/main/java/com/example/tv_shows_master/dao/TVShowDao.java
@@ -0,0 +1,29 @@
+package com.example.tv_shows_master.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.tv_shows_master.models.TVShow;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+
+
+@Dao
+public interface TVShowDao {
+
+    @Query("SELECT * FROM tvShows")
+    Flowable<List<TVShow>> getWatchlist();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addToWatchlist(TVShow tvShow);
+
+    @Delete
+    Completable removeFromWatchlist(TVShow tvShow);
+
+}
---
+++ app/src/main/java/com/example/tv_shows_master/database/TVShowsDatabase.java
@@ -0,0 +1,28 @@
+package com.example.tv_shows_master.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.tv_shows_master.models.TVShow;
+import com.example.tv_shows_master.dao.TVShowDao;
+
+@Database(entities = TVShow.class, version = 1, exportSchema = false)
+public abstract class TVShowsDatabase extends RoomDatabase {
+    private static TVShowsDatabase tvShowsDatabase;
+
+    public static synchronized TVShowsDatabase getTvShowsDatabase(Context context) {
+        if (tvShowsDatabase == null) {
+            tvShowsDatabase = Room.databaseBuilder(
+                    context,
+                    TVShowsDatabase.class,
+                    "tv_shows_db"
+            ).build();
+        }
+        return tvShowsDatabase;
+    }
+
+    public abstract TVShowDao tvShowDAO();
+}
